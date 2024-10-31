88f171a68642dc648be857d97b481bfa3bf5772c
Jee Hwan <jeehwan98@naver.com>
2024-10-31 14:57:29 +0900

    refactor: preview section

--- front/components/post/preview-section.tsx
+++ front/components/post/preview-section.tsx
@@ -1,3 +1,5 @@
+import { ContextField, PreviewSectionWrapper, TagField, TitleField } from "./ui/previewUI";
+
 interface Blog {
   title: string;
   tags: string[];
@@ -10,36 +12,10 @@ interface PreviewSectionProps {
 
 export default function PreviewSection({ blog }: PreviewSectionProps) {
   return (
-    <PreviewSectionContainer>
-      <h1 className="text-4xl font-bold mb-2">{blog.title || ""}</h1>
-      <div className="flex items-center mb-4 flex-wrap">
-        {blog.tags.map((tag, index) => (
-          <span
-            key={index}
-            className="bg-gray-200 text-green-600 px-3 py-1 rounded-full mr-2 mb-2"
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
-      <div
-        className="prose"
-        dangerouslySetInnerHTML={{
-          __html: `${blog.context}`
-        }}
-        style={{
-          overflow: 'hidden',
-          whiteSpace: 'pre-wrap',
-        }}
-      />
-    </PreviewSectionContainer>
+    <PreviewSectionWrapper>
+      <TitleField title={blog.title} />
+      <TagField tags={blog.tags} />
+      <ContextField context={blog.context} />
+    </PreviewSectionWrapper>
   )
-}
-
-function PreviewSectionContainer({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="w-1/2 p-10 border-gray-200 bg-gray-50">
-      {children}
-    </div>
-  )
-}
+};
