8583d0ef43912093f5ea03cdc2212613fa6c8375
Jee Hwan <jeehwan98@naver.com>
2024-10-31 15:31:41 +0900

    feat: link from profile header

--- front/components/header/profile/profile-header.tsx
+++ front/components/header/profile/profile-header.tsx
@@ -72,7 +72,7 @@ export function ProfileHeader({ userInfo, setUserInfo }: ProfileHeaderProps) {
       {/* dropdown menu */}
       {isOpen && (
         <DropdownMenu>
-          <HeaderProfileNavLink href="/my-blog" onClick={closeDropdown}>내 블로그</HeaderProfileNavLink>
+          <HeaderProfileNavLink href={`${userInfo?.userId}`} onClick={closeDropdown}>내 블로그</HeaderProfileNavLink>
           <HeaderProfileNavLink href="#" onClick={closeDropdown}>설정</HeaderProfileNavLink>
           <LogoutNavLink onClick={handleLogout}>로그아웃</LogoutNavLink>
         </DropdownMenu>
